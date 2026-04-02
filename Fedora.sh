07fbdac92a508c8aa3c2cd12a1e1da7520d0091c
X27 <x27@noreply.codeberg.org>
2026-04-03 01:29:01 +0200

    Update Fedora.sh

--- Fedora.sh
+++ Fedora.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # Script to choose between Fedora-Minimal.sh and Fedora-PostSetup.sh
-
 echo "Choose a script to run:"
 echo "1) Fedora-Minimal"
 echo "2) Fedora-PostSetup"
+echo "3) Fedora-Kionite-Setup"
 echo -n "Enter your choice (1 or 2): "
 read choice
 
@@ -16,6 +15,10 @@ case $choice in
     2)
         echo "Running Fedora-PostSetup.sh..."
         curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Fedora/Fedora-PostSetup.sh | bash
+        ;;
+    3)
+        echo "Running Fedora-Kionite-Setup.sh..."
+        curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Fedora/Fedora-Kionite-Setup.sh | bash
         ;;
     *)
         echo "Invalid choice. Exiting."
