9ef49605a8a9743f81a3e1c7298cd7a0e9d8e8c9
X27 <x27@noreply.codeberg.org>
2026-04-02 16:53:09 +0200

    Upload files to "Flatpak"

---
+++ Flatpak/flatpaks.sh
@@ -0,0 +1,10 @@
+#Flatpak apps
+flatpak install -y flathub \
+        org.cryptomator.Cryptomator \
+        com.bitwarden.desktop \
+        org.localsend.localsend_app \
+        com.github.zocker_160.SyncThingy \
+        org.jellyfin.JellyfinDesktop \
+        com.unicornsonlsd.finamp \
+        org.gnome.World.Iotas
+
