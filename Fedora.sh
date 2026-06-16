f1f508726edfc4b7daedaa86529e712088e7a797
X27 <x27@xlabsx27.com>
2026-06-16 22:33:29 +0200

    Removed Fedora Minimal and replaced it with the now Fedora Post-Setup

--- Fedora.sh
+++ Fedora.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
 echo "Choose a script to run:"
-echo "1) Fedora-Minimal"
-echo "2) Fedora-PostSetup"
-echo "3) Fedora-Kionite-Setup"
-echo "4) Bazzite Setup"
-echo -n "Enter your choice (1, 2, 3, or 4): "
+echo "1) Fedora Post-Setup"
+echo "2) Fedora-Kionite-Setup"
+echo "3) Bazzite Setup"
+echo -n "Enter your choice (1, 2, or 3): "
 read choice
 
 case $choice in
     1)
-        echo "Downloading and running Fedora-Minimal.sh..."
-        curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Fedora/Fedora-Minimal.sh -o /tmp/Fedora-Minimal.sh
-        chmod +x /tmp/Fedora-Minimal.sh
-        /tmp/Fedora-Minimal.sh
-        rm -f /tmp/Fedora-Minimal.sh
-        ;;
-    2)
         echo "Downloading and running Fedora-PostSetup.sh..."
         curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Fedora/Fedora-PostSetup.sh -o /tmp/Fedora-PostSetup.sh
         chmod +x /tmp/Fedora-PostSetup.sh
-        /tmp/Fedora-PostSetup.sh
+        bash /tmp/Fedora-PostSetup.sh
         rm -f /tmp/Fedora-PostSetup.sh
         ;;
-    3)
+    2)
         echo "Downloading and running Fedora-Kionite-Setup.sh..."
         curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Fedora/Fedora-Kionite-Setup.sh -o /tmp/Fedora-Kionite-Setup.sh
         chmod +x /tmp/Fedora-Kionite-Setup.sh
         /tmp/Fedora-Kionite-Setup.sh
         rm -f /tmp/Fedora-Kionite-Setup.sh
         ;;
-    4)
+    3)
         echo "Downloading and running Bazzite-Setup.sh..."
         curl -sSL https://codeberg.org/X27/X27-Linux-Desktop-Toolbox/raw/branch/main/Bazzite/Bazzite-Setup.sh -o /tmp/Bazzite-Setup.sh
         chmod +x /tmp/Bazzite-Setup.sh
